7337cd008bcca18f6d410f35ae6210a5afdd3d3e
ZDXGV1 <chzakaria191169@gmail.com>
2026-07-11 03:38:55 +0100

    fix: hardcode supabase keys for vercel deployment

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://pocijnvpsxivxvvzfiev.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvY2lqbnZwc3hpdnh2dnpmaWV2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MjU2Njc3NSwiZXhwIjoyMDk4MTQyNzc1fQ.SYZASZn6MPMIZ8jJpa3_dNLOIoHi_Uqp7snii9RWjIM"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
